d650b4c6d7b6ce9a985f8bce77120bb011bfc052
Ivonne Terrero <iqcodes21@gmail.com>
2017-03-10 13:56:31 -0800

    working on POST request

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,37 @@
 'use strict';
 
 const express = require('express');
-
-// eslint-disable-next-line new-cap
+const knex = require('../knex');
+const humps = require('humps');
 const router = express.Router();
+const bcrypt = require('bcrypt-as-promised');
 
-// YOUR CODE HERE
+
+//generating the salt. using the bcrypt.hash() func and
+// insert the emailed hashed password into the users table.
+router.post('/users', (req, res, next) => {
+  bcrypt.hash(req.body.password, 12)
+  .then((hashed_password) => {
+    // console.log(req.body.email, hashed_password);
+    return knex('users')
+    .insert({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      hashed_password: hashed_password
+    }, '*');
+  })
+  .then((users) => {
+    const user = users[0];
+    console.log('user', user);
+    console.log("this is reqbody", req.body);
+    delete user.hashed_password;
+    res.send(user);
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
+//
 
 module.exports = router;
