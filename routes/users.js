bca97cf086c611d337981dd6cceb1312f6c80ad7
Ivonne Terrero <iqcodes21@gmail.com>
2017-03-10 20:53:03 -0800

    created route to user post

--- routes/users.js
+++ routes/users.js
@@ -15,18 +15,17 @@ router.post('/users', (req, res, next) => {
     // console.log(req.body.email, hashed_password);
     return knex('users')
     .insert({
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
+      first_name: req.body.firstName,
+      last_name: req.body.lastName,
       email: req.body.email,
       hashed_password: hashed_password
     }, '*');
   })
   .then((users) => {
     const user = users[0];
-    console.log('user', user);
-    console.log("this is reqbody", req.body);
     delete user.hashed_password;
-    res.send(user);
+    res.send(humps.camelizeKeys(user));
+
   })
   .catch((err) => {
     next(err);
